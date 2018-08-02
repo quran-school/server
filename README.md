c89a32c99512f5a69ac5313a1ac3ffe950caeffb
Anas Mosaad <anas.mosaad@incorta.com>
2018-08-02 06:13:53 +0200

    Initial project setup gradle + spring + social login

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# quraan-school
+Currently the project is testing sign in services from facebook, github and google.
+Google is not working at the moment and will be investigated further.
 
-## Meeting 1 (5 July 2018)
-Below are the requirements discussed in the meeting.
-Initially we will start with the admission and registrations part. This includes regestering the students their groups and levels in which the groups study.
-- Student can be individual or belong to a familly.
-- There's a small fees paid annually for students:
-  - Indivdual students pays X.
-  - Familly students pay less. For example, first student pays X; the second member  pay X - d1; where d1 is the discount for the second member. Third members pays X - d2; where d2 is the discount for the third member and so on.
-- As required by the government, reports needs to be shown for students payments.
-  - Student total (paid vs. remaining).
-  - Familly totals (paid vs. remaining).
-  - Study Group members.
-  - School totals.
-  
-We will start with the minimal that we can deliver. We will not be going into many details that we can ignore and can only dely us.
+
+# Build instructions
+- Clone the project locally.
+- Go to the local repo path, and run `./gradlew`
+- To import the project in eclipse, please check http://www.vogella.com/tutorials/EclipseGradle/article.html
+- To import in IDEA, just go through import project wizard and choose to import from gradle.
+
+
+# Running the application
+- Execute the command `./gradlew bootRun`.
+- Go to http://localhost:8080
+- You can try login with your facebook or github account.
---
+++ src/main/java/com/letarda/quraanschool/QuraanSchoolApplication.java
@@ -0,0 +1,133 @@
+package com.letarda.quraanschool;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.CompositeFilter;
+
+import javax.servlet.Filter;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@EnableOAuth2Client
+@RestController
+public class QuraanSchoolApplication extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2ClientContext oauth2ClientContext;
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
+                .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
+                ;
+    }
+
+    private Filter ssoFilter() {
+        CompositeFilter filter = new CompositeFilter();
+        List<Filter> filters = new ArrayList<>();
+
+        filters.add(getFilter("/login/facebook", facebook(), facebookResource()));
+        filters.add(getFilter("/login/github", github(), githubResource()));
+        filters.add(getFilter("/login/google", google(), googleResource()));
+
+        filter.setFilters(filters);
+        return filter;
+    }
+
+    private OAuth2ClientAuthenticationProcessingFilter getFilter(String filterProcessesUrl,
+                                                                 AuthorizationCodeResourceDetails details,
+                                                                 ResourceServerProperties resourceServerProperties) {
+        OAuth2ClientAuthenticationProcessingFilter filter =
+                new OAuth2ClientAuthenticationProcessingFilter(filterProcessesUrl);
+        OAuth2RestTemplate template = new OAuth2RestTemplate(details, oauth2ClientContext);
+        filter.setRestTemplate(template);
+        UserInfoTokenServices tokenServices =
+                new UserInfoTokenServices(resourceServerProperties.getUserInfoUri(), details.getClientId());
+        tokenServices.setRestTemplate(template);
+        filter.setTokenServices(tokenServices);
+        return filter;
+    }
+
+    @Bean
+    @ConfigurationProperties("facebook.client")
+    public AuthorizationCodeResourceDetails facebook() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("facebook.resource")
+    public ResourceServerProperties facebookResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("github.client")
+    public AuthorizationCodeResourceDetails github() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("github.resource")
+    public ResourceServerProperties githubResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.client")
+    public AuthorizationCodeResourceDetails google() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.resource")
+    public ResourceServerProperties googleResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuraanSchoolApplication.class, args);
+    }
+}
